5f55a5f5650b2e4821fe2cbc29c895017a6458b9
Goufix <alifer.17.11@gmail.com>
2020-03-15 17:42:58 -0300

    fix tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default function Header() {
   return (
     <>
-      <Container className="d-flex justify-content-between">
+      <Container>
         <h1>Contagem de votos conquistados RCC Awards - 2020</h1>
         <hr />
       </Container>
